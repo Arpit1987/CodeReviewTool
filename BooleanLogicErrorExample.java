fc1046a4efc593e05a9924f46f1d89e4253a4e34
Arpit Khare <34269896+Arpit1987@users.noreply.github.com>
2024-02-29 18:13:52 +0530

    Create BooleanLogicErrorExample.java

---
+++ BooleanLogicErrorExample.java
@@ -0,0 +1,11 @@
+public class BooleanLogicErrorExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        boolean result;
+        
+        result = (x > y) && (x < y);
+        
+        System.out.println("The result is: " + result);
+    }
+}
